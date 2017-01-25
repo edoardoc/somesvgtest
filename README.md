88918f5d674023379b4360ce7724a8f18af0d07c
Edoardo Ceccarelli <ridleys@gmail.com>
2017-01-25 18:00:10 +0200

    corrections2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # somesvgtest with docker and nginx proxy
 
 
-`docker-compose up --build ` will launch services, `checksvgs.sh` will curl svg from different hosts showing content-type
+`docker-compose up --build ` will launch all services, `checksvgs.sh` will curl svg from different hosts showing content-type
--- checksvgs.sh
+++ checksvgs.sh
@@ -1,3 +1,5 @@
-curl -I -L -H "Host: goservesvg.local" localhost/circle.svg
-
+echo the following will show Content-Type: image/svg+xml because it is set by the "inner" nginx
 curl -I -L -H "Host: mysvgfile.local" localhost/logo_interne1.svg
+
+echo the following will show text/xml because go has not set any content type and proxy nginx does not set it...
+curl -I -L -H "Host: goservesvg.local" localhost/circle.svg
